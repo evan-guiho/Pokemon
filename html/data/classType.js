f95e5645d6980b100f8028e328fce3f6401e8ed0
Max.Verstappen4 <yoann.crogunnec@etudiant.univ-rennes.fr>
2026-03-10 15:30:44 +0100

    class type prep tab ready

---
+++ html/data/classType.js
@@ -0,0 +1,26 @@
+class Type{
+    constructor(TypeAttaque){
+        this.TypeAttaque = TypeAttaque;
+    }
+
+    prepaTableau(){
+        let TabDef = type_effectiveness[this.TypeAttaque];
+        let Dico = {};
+        for(let cle in TabDef){
+            if(TabDef[cle] in Dico){
+                Dico[cle].push(cle);
+            }
+            else{
+                Dico[cle] = [cle];
+            }
+        }
+        return Dico;
+    }
+
+    toString(){
+        let dictionnaire = this.prepaTableau();
+        
+    }
+
+    
+}
