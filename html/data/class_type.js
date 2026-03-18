036c4812a344410c3ddeea4cd05ec35289ceb7d7
Max.Verstappen4 <yoann.crogunnec@etudiant.univ-rennes.fr>
2026-03-18 09:19:31 +0100

    ajout coommentaire

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -3,16 +3,25 @@ class Type{
         this.TypeAttaque = TypeAttaque;
     }
 
+    //prépare un dictionnaire contenant l'éfficacité et les types liés
     prepaTableau(){
+        /*
+            1. Récuapration du dictionnaire liés au type
+
+            2. initialisation d'un dictionnaire
+
+            3. parcours du tableau type - efficacité
+
+            4. Si l'éfficacité appartient au dico on l'initialise
+            Sinon on ajoute le type
+        */
         let TabDef = type_effectiveness[this.TypeAttaque];
-        console.log(JSON.stringify(TabDef));
         let Dico = {};
         for(let cle in TabDef){
             let efficacite = TabDef[cle];
 
             if(!(efficacite in Dico)){
-                Dico[efficacite] = [];
-                Dico[efficacite].push(cle);
+                Dico[efficacite] = [cle];   
             }
             else{
                 Dico[efficacite].push(cle);
