067a83b6569504f83b04632e81664c78e84a008c
Max.Verstappen4 <yoann.crogunnec@etudiant.univ-rennes.fr>
2026-03-20 12:12:42 +0100

    supr commentaire inutile

--- html/data/class_attack.js
+++ html/data/class_attack.js
@@ -16,27 +16,6 @@ class Attack {
 }
 
 function fill_attacks() {
-    /*
-    for (let move of fast_moves) {
-        let typeName = pokemon_types[move.type];
-        let typeObj = all_types[typeName];
-        if(typeObj){
-            let attack = new Attack(move.id, move.name, typeObj, move.power, move.duration);    
-            Attack.all_attacks[move.id] = attack;
-        }
-    }
-
-    for (let move of charged_moves) {
-        let typeName = pokemon_types[move.type];
-        let typeObj = all_types[typeName];
-        if(typeObj){
-            let attack = new Attack(move.id, move.name, typeObj, move.power, move.duration);    
-            Attack.all_attacks[move.id] = attack;
-        }
-    }
-    C'EST QUOI CE BORDEL CHEFFFFF
-    */
-
     let liste_fast_move = fast_moves;
     let liste_charged_move = charged_moves;
 
