26d0d7bc24baf650925a6c3066194112043f6369
Max.Verstappen4 <yoann.crogunnec@etudiant.univ-rennes.fr>
2026-03-20 12:03:49 +0100

    class pokemon corriger

--- html/data/class_pokemon.js
+++ html/data/class_pokemon.js
@@ -12,6 +12,59 @@ class Pokemon {
         this.name_charged_attack = name_charged_attack;
     }
 
+    getTypes(){
+        /*
+        let all_types = {};
+        for(let type in pokemon_types){
+            let typeName = pokemon_types[type];
+            let typeObj = new Type(typeName);
+            all_types[typeName] = typeObj;
+        }
+
+        return all_types;
+        JE CAPTE MEME PAS CA FAIS QUOI CETTE MERDE typeObj C'EST QUOI CA ENCORE
+        */
+        let listeDesPokeType = [];
+        let pokeType = pokemon_types.find((item) => item.pokemon_id === this.id_pokemon).type;
+        for(type in pokeType){
+            listeDesPokeType.push(Type.all_types[type]);
+        }
+        return listeDesPokeType;
+    }
+
+    getAttacks(){
+        /*
+        let all_attacks = {};
+        for (let move of fast_moves) {
+            let typeName = pokemon_types[move.type];
+            let typeObj = all_types[typeName];
+            if(typeObj){
+                let attack = new Attack(move.id, move.name, typeObj, move.power, move.duration);    
+                all_attacks[move.id] = attack;
+            }
+        }
+
+        for (let move of charged_moves) {
+            let typeName = pokemon_types[move.type];
+            let typeObj = all_types[typeName];
+            if(typeObj){
+                let attack = new Attack(move.id, move.name, typeObj, move.power, move.duration);    
+                all_attacks[move.id] = attack;
+            }
+        }
+        return all_attacks;
+        */
+        let liste_attack = [];
+
+        let attack_rapide = Attack.all_attacks[this.name_fast_attack];
+        let attack_lente = Attack.all_attacks[this.name_charged_attack];
+
+        liste_attack.push(attack_lente);
+        liste_attack.push(attack_rapide);
+
+        return liste_attack;
+    }
+
     toString() {
         return this.name + " : #" + this.id_pokemon + ", [" + this.type_name + "], " + "[ STA: " + this.stamina + ", ATK: " + this.base_attack + ", DEF:" + this.base_defense + "], Rapides = " +  this.name_fast_attack + ", Chargés = " + this.name_charged_attack + "";
     }
@@ -19,35 +72,5 @@ class Pokemon {
     
 }
 
-function getTypes(){
-    let all_types = {};
-    for(let type in pokemon_types){
-        let typeName = pokemon_types[type];
-        let typeObj = new Type(typeName);
-        all_types[typeName] = typeObj;
-    }
 
-    return all_types;
-}
 
-function getAttacks(){
-    let all_attacks = {};
-    for (let move of fast_moves) {
-        let typeName = pokemon_types[move.type];
-        let typeObj = all_types[typeName];
-        if(typeObj){
-            let attack = new Attack(move.id, move.name, typeObj, move.power, move.duration);    
-            all_attacks[move.id] = attack;
-        }
-    }
-
-    for (let move of charged_moves) {
-        let typeName = pokemon_types[move.type];
-        let typeObj = all_types[typeName];
-        if(typeObj){
-            let attack = new Attack(move.id, move.name, typeObj, move.power, move.duration);    
-            all_attacks[move.id] = attack;
-        }
-    }
-    return all_attacks;
-}
