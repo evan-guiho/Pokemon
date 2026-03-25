09c33845eac0e000ebdd6ee80dadafdf6346b8ef
Max.Verstappen4 <yoann.crogunnec@etudiant.univ-rennes.fr>
2026-03-25 08:51:29 +0100

    ajout protection

--- html/data/partie_tests.js
+++ html/data/partie_tests.js
@@ -9,21 +9,28 @@ function getPokemonsByType(typeName){
     afficher si le type est bon le poke
     sinon rien    
     */
-    estChaineCarac(typeName);
-    let boucle = 1;
-    let all_poke = Pokemon.all_pokemons;
+    chaine = estChaineCarac(typeName);
+    if(typeof chaine == "string"){
 
-    for(let poke in all_poke){
-        for(let i = 0; i < all_poke[poke].type_name.length; i++){
-            if(all_poke[poke].type_name[i] == typeName){
-                if(boucle === 1){
-                    console.log("Voici le "+all_poke[poke].toString()+" du "+boucle+"er pokémon");
+        let boucle = 1;
+        let all_poke = Pokemon.all_pokemons;
+
+        for(let poke in all_poke){
+            for(let i = 0; i < all_poke[poke].type_name.length; i++){
+                if(all_poke[poke].type_name[i] == typeName){
+                    if(boucle === 1){
+                        console.log("Voici le "+all_poke[poke].toString()+" du "+boucle+"er pokémon");
+                    }
+                    else{
+                        console.log("Voici le "+all_poke[poke].toString()+" du "+boucle+"éme pokémon");
+                    }
+                    boucle += 1;
                 }
-                else{
-                    console.log("Voici le "+all_poke[poke].toString()+" du "+boucle+"éme pokémon");
-                }
-                boucle += 1;
             }
-        }
+        }    
     }
+    else{
+        console.log("Impossible de chercher. erreur de saisie. PANIC !!!!");
+    }
+    
 }
