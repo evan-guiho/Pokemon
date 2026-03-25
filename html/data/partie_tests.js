c15278b892182b810c882762a4726853ba26e1ac
evan-guiho <evan.giho@gmail.com>
2026-03-25 08:55:00 +0100

    getPokemonByAttack créa

--- html/data/partie_tests.js
+++ html/data/partie_tests.js
@@ -34,3 +34,38 @@ function getPokemonsByType(typeName){
     }
     
 }
+
+function getPokemonsByAttack(attackName){
+    /*
+    recup vriable all_pke
+    chercher dedans les poks avec le type demander
+    chercher dans la liste des type du poke
+    afficher si le type est bon le poke
+    sinon rien    
+    */
+    chaine = estChaineCarac(attackName);
+    if(typeof chaine == "string"){
+
+        let boucle = 1;
+        let all_poke = Pokemon.all_pokemons;
+
+        for(let poke in all_poke){
+            for(let i = 0; i < all_poke[poke].attackName.length; i++){
+                if(all_poke[poke].attackName[i] == typeName){
+                    if(boucle === 1){
+                        console.log("Voici le "+all_poke[poke].toString()+" du "+boucle+"er pokémon");
+                    }
+                    else{
+                        console.log("Voici le "+all_poke[poke].toString()+" du "+boucle+"éme pokémon");
+                    }
+                    boucle += 1;
+                }
+            }
+        }    
+    }
+    else{
+        console.log("Impossible de chercher. erreur de saisie. PANIC !!!!");
+    }
+    
+}
+
