c1011779f6ee882ce559a00c029b5159a778dab8
evan-guiho <evan.giho@gmail.com>
2026-04-01 10:43:23 +0200

    Test js fonctionnel pour que ça fonctionne
    Certainement a changé d'ailleur ça me parrait sus

--- html/test/test.js
+++ html/test/test.js
@@ -2,7 +2,6 @@ fill_types();
 fill_attacks();
 fill_pokemon();
 
-// Squirtle (Water) vs Charmander (Fire) → Water est super efficace contre Fire (×1.6)
 const pokemonA = Object.values(Pokemon.all_pokemons).find((p) => p.name === "Squirtle");
 const pokemonNameB = "Charmander";
 getBestFastAttacksForEnemy.call(pokemonA, true, pokemonNameB);
